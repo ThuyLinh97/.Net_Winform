fb35338606b335240d0d3f48897997375aa94354
Nguyễn Thùy Linh <55010260+ThuyLinh97@users.noreply.github.com>
2019-10-11 09:11:54 +0700

    Add files via upload

---
+++ AppG22019-master/AppG2/Controller/StudentService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppG2.Model;
+namespace AppG2.Controller
+{
+    public class StudentService
+    {
+        /// <summary>
+        /// Lấy sinh viên theo mã sinh viên từ MockData
+        /// </summary>
+        /// lấy sv từ file
+        /// <param name="pathDataFile">đường dẫn tới file chứa dư xlieeuj</param>
+        /// <param name="idStudent">Ma sv</param>
+        /// <returns>Sinh viên có mã tương ứng hoặc null</returns>
+        public static Student GetStudent(string idStudent)
+        {
+            Student student = new Student
+            {
+                IDStudent = idStudent,
+                FirstName = "Linh",
+                LastName = "Nguyễn",
+                DOB = new DateTime(1997, 12,01),
+                POB = "Quang Binh",
+                Gender = GENDER.Female
+            };
+            student.ListHistoryLearning = new List<HistoryLearning>();
+            for(int i = 1 ; i <=12; i++)
+            {
+                HistoryLearning historyLearning = new HistoryLearning
+                {
+                    IDHistoryLearning = i.ToString(),
+                    YearFrom = 2006 + i,
+                    YearEnd = 2007 + i,
+                    Address = "THCS Phan Bội Châu",
+                    IDStudent = idStudent,
+                };
+                student.ListHistoryLearning.Add(historyLearning);
+            }
+            return student;
+        }
+        public static Student GetStudent(string pathDataFile,string idStudent)
+        {
+            if (File.Exists(pathDataFile))
+            {
+                CultureInfo culture = CultureInfo.InvariantCulture;
+                var listLines = File.ReadAllLines(pathDataFile);
+                foreach(var line in listLines)
+                {
+                    var rs = line.Split(new char[] { '#' });
+                    Student student = new Student
+                    {
+                        IDStudent = rs[0],
+                        FirstName = rs[1],
+                        LastName = rs[2],
+                        Gender = rs[3] == "Male" ? GENDER.Male : (rs[3] == "Female" ? GENDER.Female : GENDER.Other),
+                        DOB = DateTime.ParseExact(rs[4], "yyyy-MM-dd", culture),
+                        POB = rs[5]
+                    };
+                    if (student.IDStudent == idStudent)
+                        return student;
+                  
+                }
+                return null;
+            }
+            return null;
+        }
+    }
+}
---
+++ AppG22019-master/AppG2/View/frmThongTinSinhVien.cs
@@ -0,0 +1,125 @@
+﻿using AppG2.Controller;
+using AppG2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppG2.View
+{
+    public partial class frmThongTinSinhVien : Form
+    {
+        #region Variables to process Image Avatar 
+        Image image;
+        string pathDirectoryImg;
+        string pathAvatarImg;
+        #endregion
+
+        string pathStudentDataFile;
+
+        public frmThongTinSinhVien(string maSinhVien)
+        {
+            InitializeComponent();
+            pathDirectoryImg = Application.StartupPath + "\\Img";
+            pathAvatarImg = pathDirectoryImg + "\\avatar.png";
+            picAnhDaiDien.AllowDrop = true;
+            pathStudentDataFile = Application.StartupPath + @"\Data\student.txt";
+            if (File.Exists(pathAvatarImg))
+            {
+                FileStream fileStream = new FileStream(pathAvatarImg, FileMode.Open, FileAccess.Read);
+                picAnhDaiDien.Image = Image.FromStream(fileStream);
+                fileStream.Close();
+            }
+
+            bdsQuaTrinhHocTap.DataSource = null;
+            dtgQuaTrinhHocTap.AutoGenerateColumns = false;
+            //var student=StudentService.GetStudent(masinhvien)
+            var student = StudentService.GetStudent(pathStudentDataFile,maSinhVien);
+           
+            if (student == null)
+                throw new Exception("Không tồn tại sinh viên này");
+            else
+            {
+                txtMaSinhVien.Text = student.IDStudent;
+                txtHo.Text = student.LastName;
+                txtTen.Text = student.FirstName;
+                txtQueQuan.Text = student.POB;
+                dtpNgaySinh.Value = student.DOB;
+                chkGioiTinh.Checked = student.Gender == GENDER.Male;
+                if (student.ListHistoryLearning != null)
+                {
+                    bdsQuaTrinhHocTap.DataSource = student.ListHistoryLearning;
+                }
+            }
+            dtgQuaTrinhHocTap.DataSource = bdsQuaTrinhHocTap;
+        }
+
+        private void LnkChonAnhDaiDien_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Chọn ảnh đại diện";
+            openFileDialog.Filter = "File ảnh(*.png, *.jpg)|*.png;*.jpg";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(openFileDialog.FileName);
+                picAnhDaiDien.Image = image;
+            }
+        }
+
+        private void BtnCapNhat_Click(object sender, EventArgs e)
+        {
+            #region Cập nhật hình đại diện
+            bool imageSave = false;
+            if (image != null)
+            {
+                if (!Directory.Exists(pathDirectoryImg))
+                {
+                    Directory.CreateDirectory(pathDirectoryImg);
+                }
+                image.Save(pathAvatarImg);
+                imageSave = true;
+            }
+            #endregion
+
+            if (imageSave)
+            {
+                MessageBox.Show(
+                    "Đã cập nhật thông tin sinh viên thành công",
+                    "Thông báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                    );
+            }
+        }
+
+        private void PicAnhDaiDien_DragDrop(object sender, DragEventArgs e)
+        {
+            var rs = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var filePath = rs.FirstOrDefault();
+            image = Image.FromFile(filePath);
+            picAnhDaiDien.Image = image;
+        }
+
+        private void PicAnhDaiDien_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void MniXoaAnhDaiDien_Click(object sender, EventArgs e)
+        {
+            picAnhDaiDien.Image = Properties.Resources.avatar;
+            File.Delete(pathAvatarImg);
+        }
+
+        private void FrmThongTinSinhVien_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
